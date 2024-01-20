6ba31a745c4d9cc81afc60848f51536d3e752e94
zarusoba1116 <zarusoba327@gmail.com>
2024-01-21 01:49:41 +0900

    a

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    elif message.channel.id in [1189922398049402890, 1183748739366662176, 876362300632760344]:
+    elif message.channel.id in [1189922398049402890, 1183748739366662176, 876362300632760342]:
         if message.mentions:
             for user_mention in message.mentions:
                 with open('data.json', 'r') as json_open:
