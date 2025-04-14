7ffb05e408e0bc5b6595e192ff5bf49381698f31
kensei <kensee327@gmail.com>
2025-04-15 02:45:26 +0900

    asd

--- keep_alive.py
+++ keep_alive.py
@@ -8,10 +8,9 @@ app = Flask('')
 def home():
     return "I'm alive"
 
-def run():
-    port = int(os.environ.get("PORT", 8080))  # 環境変数からポートを取得
+def run(port):
     app.run(host="0.0.0.0", port=port)
 
-def keep_alive():
-    t = Thread(target=run)
+def keep_alive(port=8080):
+    t = Thread(target=run, args=(port,))
     t.start()
